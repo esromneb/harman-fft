33bfdf5d4d79960471f7e0cba6cc4be19e240f11
Benjamin Morse <bjmorse@gmail.com>
2015-07-11 21:56:45 -0700

    * added library

---
+++ HKWSimple/HKWSimple/FFTViewController.h
@@ -0,0 +1,13 @@
+//
+//  FFTViewController.h
+//  HKWSimple
+//
+//  Created by x on 7/11/15.
+//  Copyright (c) 2015 Harman International. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FFTViewController : NSObject
+
+@end
---
+++ HKWSimple/HKWSimple/FFTViewController.m
@@ -0,0 +1,13 @@
+//
+//  FFTViewController.m
+//  HKWSimple
+//
+//  Created by x on 7/11/15.
+//  Copyright (c) 2015 Harman International. All rights reserved.
+//
+
+#import "FFTViewController.h"
+
+@implementation FFTViewController
+
+@end
